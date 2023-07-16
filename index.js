2fb9792981e160538574113b958b34c479b0ae49
Alexey A. Taran <taran.alexey@gmail.com>
2023-07-16 19:50:41 +0300

    Levels support

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 import { FLAG, MineSweeper } from "./minesweeper.js";
 import { createContainer, createTile, updateTile } from "./render.js";
 
-function main() {
-  const width = 16;
-  const height = 16;
-  const minesCount = 40;
-
-  const placeholder = document.querySelector("#placeholder");
-  const counter = document.querySelector("#counter");;
-  const actionSelector = document.querySelector("#actionSelector");
-
-  if (!placeholder || !counter || !actionSelector) {
-    throw new Error("UI not found");
+/**
+ * 
+ * @param {number} width 
+ * @param {number} height 
+ * @param {number} minesCount 
+ * @param {Element} placeholder 
+ * @param {Element} counter 
+ * @param {HTMLInputElement} actionSelector 
+ */
+function startGame(width, height, minesCount, placeholder, counter, actionSelector) {
+  if (placeholder.childElementCount > 0) {
+    for (const child of placeholder.children) {
+      placeholder.removeChild(child);
+    }
   }
-  
-  
+
   const game = new MineSweeper(width, height, minesCount);
   const container = createContainer(width, height);
   const field = game.field;
@@ -62,6 +64,48 @@ function main() {
   counter.textContent = minesCount.toString();
 
   placeholder.appendChild(container);
+}
+
+function main() {
+  const width = 16;
+  const height = 16;
+  const minesCount = 40;
+
+  const placeholder = document.querySelector("#placeholder");
+  const counter = document.querySelector("#counter");
+  const actionSelector = document.querySelector("#actionSelector");
+
+  /**
+   * @type {HTMLButtonElement | null}
+   */
+  const beginnerButton = document.querySelector("#beginner");
+  /**
+   * @type {HTMLButtonElement | null}
+   */
+  const intermidiateButton = document.querySelector("#intermidiate");
+  /**
+   * @type {HTMLButtonElement | null}
+   */
+  const expertButton = document.querySelector("#expert");
+
+  if (!placeholder || !counter || !actionSelector||
+    !beginnerButton || !intermidiateButton || !expertButton) {
+    throw new Error("UI not found");
+  }
+  
+  beginnerButton.onclick = () => {
+    startGame(9, 9 ,10, placeholder, counter, actionSelector);
+  }
+  
+  intermidiateButton.onclick = () => {
+    startGame(16, 16 ,40, placeholder, counter, actionSelector);
+  }
+  
+  expertButton.onclick = () => {
+    startGame(30, 16 ,99, placeholder, counter, actionSelector);
+  }
+  
+  startGame(9, 9 ,10, placeholder, counter, actionSelector);
 }
 
 main();
